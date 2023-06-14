ffcec8dc68d7d4a8e117b8d9d6838c5b19f7f6a7
Ganapath1 <99318790+Ganapath1@users.noreply.github.com>
2023-06-14 12:28:53 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,16 @@
-def call(String project, String ImageTag, String hubUser){
+// def call(String project, String ImageTag, String hubUser){
+//   sh """
+//     docker build -t ${hubUser}/${project} .
+//     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+//     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+//   """
+// }
+
+def call(String aws_account_id, String region, String EcrRepoUser){
+
   sh """
-    docker build -t ${hubUser}/${project} .
-    docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-    docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+      docker build -t ${EcrRepoUser} .
+      docker tag ${EcrRepoUser}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${EcrRepoUser}:latest
   """
+  
 }
