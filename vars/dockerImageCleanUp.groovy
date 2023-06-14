38678380a5d6dbdb4dda0af9b6186a3591aa40b9
Ganapath1 <99318790+Ganapath1@users.noreply.github.com>
2023-06-14 14:34:16 +0530

    Update dockerImageCleanUp.groovy

--- vars/dockerImageCleanUp.groovy
+++ vars/dockerImageCleanUp.groovy
@@ -1,8 +1,16 @@
-def call(String project, String ImageTag, String hubUser){
+// def call(String project, String ImageTag, String hubUser){
+
+//   sh """
+//     docker rmi ${hubUser}/${project}:${ImageTag}
+//     docker rmi ${hubUser}/${project}:latest
+//   """
+
+// }
+
+def call(String aws_account_id, String region, String ecrRepoUser){
 
   sh """
-    docker rmi ${hubUser}/${project}:${ImageTag}
-    docker rmi ${hubUser}/${project}:latest
+      docker rmi ${ecrRepoUser}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecrRepoUser}:latest
   """
 
 }
